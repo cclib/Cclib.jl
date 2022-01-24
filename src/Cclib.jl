a7f4dd239d3eb22f98c0ba2da0b4fa53c5934fd6
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2022-01-24 14:28:18 -0300

    Write a working prototype
    Make PyCall a dependency
    Initialize the global cclib object
    Add a docstring
    Precompile module

--- src/Cclib.jl
+++ src/Cclib.jl
@@ -1,5 +1,27 @@
+# Basically an exact application of
+# <https://github.com/JuliaPy/PyCall.jl#using-pycall-from-julia-modules>.
+#
+# The initialization strategy is similar to the one employed by SymPy.jl, see
+# <https://github.com/JuliaPy/SymPy.jl/blob/aea9fb5568022bbd68fe4b69d670ce2a22413a7c/src/SymPy.jl#L124>.
+
+__precompile__()
+
 module Cclib
 
-# Write your package code here.
+export cclib
+
+using PyCall
+
+"""
+    cclib
+
+Variable from `pyimport("cclib")`.
+Numerous methods are available through Python's dot-call syntax.
+"""
+const cclib = PyCall.PyNULL()
+
+function __init__()
+    copy!(cclib, PyCall.pyimport_conda("cclib", "cclib"))
+end
 
 end
