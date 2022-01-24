9f2f1672759381fe3bf97b12dc840698c2052a8c
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2022-01-24 14:31:28 -0300

    Add tests
    Add simple tests as per <https://cclib.github.io/how_to_parse.html\#from-python>
    Also test initial coordinates

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,22 @@ using Cclib
 using Test
 
 @testset "Cclib.jl" begin
-    # Write your tests here.
+    filename = "water.out"
+
+    @testset "Parsing a log file with ccopen" begin
+        parser = cclib.io.ccopen(filename)
+        data = parser.parse()
+
+        @test data.natom == 3
+        @test data.nmo == 7
+        @test data.atomcoords[1, :, :] == [0.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+    end
+
+    @testset "Parsing a log file with ccread" begin
+        data = cclib.io.ccread(filename)
+
+        @test data.natom == 3
+        @test data.nmo == 7
+        @test data.atomcoords[1, :, :] == [0.0 0.0 0.0; 0.0 1.0 0.0; 0.0 0.0 1.0]
+    end
 end
