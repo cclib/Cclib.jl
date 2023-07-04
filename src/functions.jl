964293687a09c57f41a5b31bbe9361e53365cb17
Victor Cano Gil <vcanogil@gmail.com>
2023-07-04 16:01:00 -0400

    add more data samples, add type dict

--- src/functions.jl
+++ src/functions.jl
@@ -3,6 +3,82 @@ using CondaPkg
 
 export ccread
 
+cclibtypes = Dict(
+    "aonames" => Array{String},
+    "aooverlaps" => Array{Float64, 2},
+    "atombasis" => Array{Array{Int64}},
+    "atomcharges" => Dict{String, Array{Float64}},
+    "atomcoords" => Array{Float64, 3},
+    "atommasses" => Array{Float64},
+    "atomnos" => Array{Int64},
+    "atomspins" => Dict{Array},
+    "ccenergies" => Array{Float64, 2},
+    "charge" => Int64,
+    "coreelectrons" => Array{}, # specify subytpe?
+    "dispersionenergies" => Array{Float64},
+    "enthalpy" => Float64,
+    "entropy" => Float64,
+    "etenergies" => Array{Float64},
+
+    # subtypes for these?
+    "etoscs" => Array{},
+    "etdips" => Array{},
+    "etveldips" => Array{},
+    "etmagdips" => Array{},
+    "etrotats" => Array{},
+
+    "etsecs" => Array{Array},
+    "etsyms" => Array{String},
+    "freeenergy" => Float64,
+    "fonames" => Array{String},
+    "fooverlaps" => Array{}, # subtype ?
+    "fragnames" => Array{String},
+    "frags" => Array{Array},
+
+    "gbasis" => Any{}, # PyQuante format
+    "geotargets" => Array{},
+    "geovalues" => Array{},
+    "grads" => Array{},
+    "hessian" => Array{},
+    "homos" => Array{},
+    "metadata" => Dict{Any, Any},
+    "mocoeffs" => Array{Array{Float64}},
+    "moenergies" => Array{Array{Float64}},
+    "moments" => Array{Array},
+    "mosysm" => Array{Array},
+    "mpenergies" => Array{Array{Float64}},
+    "mult" => Int64,
+    "natom" => Int64,
+    "nbasis" => Int64,
+    "nmo" => Int64,
+    "nmrtensors" => Any{}, # Find the right one for this one
+    "nocoeffs" => Array{Array},
+    "nooccnos" => Array{Array},
+    "optdone" => Bool,
+    "optstatus" => Array{},
+    "polarizabilities" => Array{Array},
+    "pressure" => Float64,
+    "scancoords" => Array{},
+    "scanenergies" => Array{},
+    "scannames" => Array{String},
+    "scanparm" => Array{Tuple},
+    "scfenergies" => Array{},
+    "scftargets" => Array{},
+    "scfvalues" => Array{},
+    "temperature" => Float64,
+    "time" => Array{},
+    "transprop" => Dict{Any, Any},
+    "vibanharms" => Array{},
+    "vibdisps" => Array{},
+    "vibfreqs" => Array{},
+    "vibfconsts" => Array{},
+    "vibirs" => Array{},
+    "vibanharms" => Array{},
+    "vibrmasses" => Array{},
+    "vibsyms" => Array{String},
+    "zpve" => Float64,
+)
+
 function ccread(file)
     data = cclib[].io.ccread(file)
     keys = pyconvert(Array, data.__dict__.keys())
