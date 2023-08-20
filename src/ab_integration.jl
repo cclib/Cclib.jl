a900e9ae39de2dfede1a1d92b91ccb86face5f23
Victor Hugo Cano Gil <49959310+vcanogil@users.noreply.github.com>
2023-08-20 16:32:13 -0400

    Dev/atomsbase update (#35)
    * update
    
    * update functions
    
    * update docs, add funcs and tests
    
    * fix doc typo
    
    * add commas
    
    * update typos

--- src/ab_integration.jl
+++ src/ab_integration.jl
@@ -2,6 +2,9 @@
 # Functions for AtomsBase integration https://github.com/JuliaMolSim/AtomsBase.jl/tree/master
 #
 export get_atom_objects
+export make_flexible_system
+export make_isolated_system
+export update_system
 
 """
     get_atom_objects(mol::Dict)
@@ -38,4 +41,151 @@ A list of AtomsBase atom objects
 """
 function get_atom_objects(mol::String)
     return get_atom_objects(ccread(mol))
+end
+
+"""
+    make_flexible_system(mol::Dict)
+
+Create an AtomsBase FlexibleSystem.
+
+# Arguments
+- `mol::Dict`: Dictionary that was returned by `ccread` function
+- `args` will typically include `bounding_box` and `boundary_conditions`
+- `kwargs` passed will be stored as custom properties of the system
+
+# Returns
+An AtomsBase FlexibleSystem
+
+# Example
+```Julia
+julia> box = [[3.0, 0.0, 0.0], [0.0, 3.0, 0.0], [0.0, 0.0, 3.0]]u"Å"
+julia> boundary_conditions = [Periodic(), Periodic(), Periodic()]
+julia> myownproperty = "hello"
+julia> mol = make_flexible_system(
+                                 ccread("water_mp2.out"),
+                                 box,
+                                 boundary_conditions
+                                 myownproperty = myownproperty
+                                 )
+FlexibleSystem(H₂O, periodic = TTT):
+bounding_box      : [       3        0        0;
+    0        3        0;
+    0        0        3]u"Å"
+myownproperty     : hello
+
+Atom(O,  [       0,        0, -0.0666785]u"Å")
+Atom(H,  [       0, -0.790649, 0.529118]u"Å")
+Atom(H,  [       0, 0.790649, 0.529118]u"Å")
+
+.------.
+/|      |
+O |      |
+| |      |
+|H.------.
+|H      /
+*------*
+```
+"""
+function make_flexible_system(mol::Dict, args...; kwargs...)
+    atoms = get_atom_objects(mol)
+    return FlexibleSystem(atoms, args...; kwargs...)
+end
+
+"""
+    make_flexible_system(mol::String)
+
+Create an AtomsBase FlexiblySystem.
+
+# Arguments
+- `mol::String`: File supported by `ccread` function
+- `args` will typically include `bounding_box` and `boundary_conditions`
+- `kwargs` passed will be stored as custom properties of the system
+
+# Returns
+An AtomsBase FlexibleSystem
+
+# Example
+```Julia
+julia> box = [[3.0, 0.0, 0.0], [0.0, 3.0, 0.0], [0.0, 0.0, 3.0]]u"Å"
+julia> boundary_conditions = [Periodic(), Periodic(), Periodic()]
+julia> myownproperty = "hello"
+julia> mol = make_flexible_system(
+                                 "water_mp2.out",
+                                 box,
+                                 boundary_conditions
+                                 myownproperty = myownproperty
+                                 )
+FlexibleSystem(H₂O, periodic = TTT):
+bounding_box      : [       3        0        0;
+    0        3        0;
+    0        0        3]u"Å"
+myownproperty     : hello
+
+Atom(O,  [       0,        0, -0.0666785]u"Å")
+Atom(H,  [       0, -0.790649, 0.529118]u"Å")
+Atom(H,  [       0, 0.790649, 0.529118]u"Å")
+
+.------.
+/|      |
+O |      |
+| |      |
+|H.------.
+|H      /
+*------*
+```
+"""
+function make_flexible_system(mol::String, args...; kwargs...)
+    return(make_flexible_system(ccread(mol), args...; kwargs...))
+end
+
+"""
+    make_isolated_system(mol::Dict)
+
+Create an AtomsBase isolated_system.
+
+# Arguments
+- `mol::Dict`: Dictionary that was returned by `ccread` function
+- `kwargs` passed will be stored as custom properties of the system
+
+# Returns
+An AtomsBase isolated_system
+"""
+function make_isolated_system(mol::Dict; kwargs...)
+    atoms = get_atom_objects(mol)
+    return isolated_system(atoms; kwargs...)
+end
+
+"""
+    make_isolated_system(mol::String)
+
+Create an AtomsBase isolated_system.
+
+# Arguments
+- `mol::String`: File supported by `ccread` function
+- `kwargs` passed will be stored as custom properties of the system
+
+# Returns
+An AtomsBase isolated_system
+"""
+function make_isolated_system(mol::String; kwargs...)
+    return make_isolated_system(ccread(mol); kwargs...)
+end
+
+"""
+    update_system(system::AbstractSystem)
+
+Update the system by changing and/or adding new properties,
+which are given as `kwargs`.
+Supported `kwargs` include `particles`, `atoms`, `bounding_box`,
+and `boundary_conditions`. User-specified `kwargs` are passed as
+custom properties.
+
+# Arguments
+- `system::AbstractSystem`: An AtomsBase System
+
+# Returns
+An updated AtomsBase system
+"""
+function update_system(system; kwargs...)
+    return AbstractSystem(system; kwargs...)
 end
